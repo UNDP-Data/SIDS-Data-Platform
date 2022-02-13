b36b1777fff3cbd9e58af9c2b05d6631f81ea69c
Lenseg <Lenseg1@gmail.com>
2022-02-13 19:11:59 +0000

    Deploying to gh-pages from @ SIDS-Dashboard/SIDSDataPlatform@c147da222df0be47cf369da6be922577a4f8b8f8 🚀

--- src/gis/static/filepaths.js
+++
@@ -1,5 +0,0 @@
-const filepaths = {
-  cablesFilePath: `${process.env.BASE_URL}static/cable-geo.json`,
-  pointdataFilePath: `${process.env.BASE_URL}static/pvaph.geojson`,
-};
-export default filepaths;
--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/SIDSDataPlatform/'
-    : '/',
-  outputDir:'docs',
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
