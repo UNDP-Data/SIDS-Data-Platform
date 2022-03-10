b08d53c6269bc8fa315bb3ed92f27cbed2483cdd
Lenseg <Lenseg1@gmail.com>
2022-03-10 13:15:15 +0000

    Deploying to gh-pages from @ SIDS-Dashboard/SIDSDataPlatform@5cd02d7a2dbb0d909fe4cae605898d6c8e67ce20 🚀

--- js/index.js
+++ js/index.js
@@ -268,7 +268,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\n//\n//\n/* ha
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: 'RootHeader',\n  computed: {\n    isTesting: function isTesting() {\n      console.log(\"staging\");\n      return \"staging\" === 'testing';\n    }\n  }\n});\n\n//# sourceURL=webpack:///./src/components/RootHeader.vue?./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options");
+eval("__webpack_require__.r(__webpack_exports__);\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n  name: 'RootHeader',\n  computed: {\n    isTesting: function isTesting() {\n      return \"staging\" === 'testing';\n    }\n  }\n});\n\n//# sourceURL=webpack:///./src/components/RootHeader.vue?./node_modules/cache-loader/dist/cjs.js??ref--13-0!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--1-0!./node_modules/vue-loader/lib??vue-loader-options");
 
 /***/ }),
 
