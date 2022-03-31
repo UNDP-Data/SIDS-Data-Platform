cc6a6be7c1089285de97e70021c99f35a2da4967
Антон Степаненков <lens@Air-Anton-2.home>
2022-03-31 18:28:37 +0300

    fix: screen size change fix

--- src/choro/timeSeries.js
+++ src/choro/timeSeries.js
@@ -641,6 +641,7 @@ export function updateTimeChart({ dataset, optionSelected }) {
     } else {
         g.selectAll("line.link").remove()
         g.selectAll("line.h").remove()
+        g.selectAll("text.legend").remove()
     }
   }
 
