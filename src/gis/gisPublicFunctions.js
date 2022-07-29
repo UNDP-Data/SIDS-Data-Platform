f94207ef9639b5eaacec2c83fcd37236478797c2
Антон Степаненков <lenseg1@gmail.com>
2022-07-29 17:45:35 +0300

    loading fix

--- src/gis/gisPublicFunctions.js
+++ src/gis/gisPublicFunctions.js
@@ -176,6 +176,7 @@ export function updateData(
         if (!comparison) {
           self.addNoDataLegend(activeLayer);
         }
+        this.emit('loadingStart')
       }
       map.once('idle', () => {
         if(this.options.mode3d) {
