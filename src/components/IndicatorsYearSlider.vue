9be6bc6617ac3547bdf157e961032e0f227ff98e
Roxana Torre <roxana@torre.nl>
2023-07-03 18:11:34 +0200

    improved slider styles

--- src/components/IndicatorsYearSlider.vue
+++ src/components/IndicatorsYearSlider.vue
@@ -1,17 +1,20 @@
 <template>
-  <!-- div class='sliderDiv' v-if="(indiCode !== 'region') && (activeIndicatorYears.length > 1)" -->
-  <div class='sliderDiv'></div>
-  <!-- div style='text-align: center;' v-else-if="(activeIndicatorYears.length === 1)"><h4>Year: {{ activeIndicatorYears[0] }}</h4></div -->
+  <div class="sliderContainer">
+    <div class='sliderDiv'></div>
+    <h4>Year: {{ activeIndicatorYears[0] }}</h4>
+  </div>
 
 </template>
 <script>
 /*global gtag*/
-import { mapState } from 'vuex';
+import { mapState} from 'vuex';
 import * as d3 from 'd3';
-import {sliderBottom} from 'd3-simple-slider';
+import {sliderTop} from 'd3-simple-slider';
 // eslint-disable-next-line no-unused-vars
 var selectedYear;
-var slider, sliderSVG;
+var slider, sliderSVG; 
+// eslint-disable-next-line no-unused-vars
+var hideSlider = false;
 
 export default {
   name: 'IndicatorsYearSlider',
@@ -27,10 +30,6 @@ export default {
     }),
     activeIndicatorYears(){
       if(this.data && this.data.data) {
-        const test = Object.keys(this.data.data).filter(year => (year !== 'recentYear') && year !== 'recentValue').map(year => {
-          return Number(year)
-        })
-        console.log('test', test)
         return Object.keys(this.data.data).filter(year => (year !== 'recentYear') && year !== 'recentValue').map(year => {
           return Number(year)
         })
@@ -42,7 +41,6 @@ export default {
       let array = [];
       let min = Math.min(...this.activeIndicatorYears);
       let max = Math.max(...this.activeIndicatorYears);
-      console.log('min',min,'max',max)
       for (let i = min; i <= max; i++) array.push(i);
       return array;
     }
@@ -60,18 +58,31 @@ export default {
         this.$emit('yearChange', year)
       }
     },
+    resetTicks(){
+      sliderSVG.select('.axis')
+        .attr('transform','translate(0,0)');
+
+      let ticks = sliderSVG.selectAll('.tick');
+
+      ticks.append('circle')
+        .attr('stroke','var(--dark-red)')
+        .attr('fill','#fff')
+        .attr('r',3);
+
+      ticks.select('text').attr('y', -10);
+      sliderSVG.select('.track-fill').attr('x1',-8)
+    }
   },
   async mounted(){
-    console.log('mounted')
     await this.defaultYear();
-    slider = sliderBottom()
-    .tickValues(this.activeIndicatorYears)
+    slider = sliderTop()
+    //.tickValues(this.activeIndicatorYears)
     .min(this.sliderYearsArray[0])
     .max(this.sliderYearsArray[this.sliderYearsArray.length - 1])
     .width(700)
-    // .displayValue(true)
+    .displayValue(true)
     .tickFormat(d3.format('d'))
-    .fill('var(--gray-400)')
+    .fill('var(--dark-red)')
     .value(this.sliderYearsArray[this.sliderYearsArray.length - 1])
     .marks(this.sliderYearsArray)
     .handle(['M -10, 0 a 10,10 0 1,1 20,0 a 10,10 0 1,1 -20,0'])
@@ -83,22 +94,50 @@ export default {
       .attr('height', 100)
       .append('g')
       .attr('transform', 'translate(30,30)')
-      .call(slider)
+    
+    if (this.sliderYearsArray.length > 1 && this.indiCode !== 'region') {
+      sliderSVG.call(slider)
+      hideSlider = false;
+      this.resetTicks();
+      sliderSVG.select('.parameter-value')
+      .insert('rect', 'text')
+      .attr('x',-20)
+      .attr('y',-44)
+      .attr('rx', 5)
+      .attr('width', 40)
+      .attr('height', 25)
+      .attr('fill', '#000');
+
+      sliderSVG.select('.parameter-value')
+      .append('polygon')
+        .attr('points','-10,-24 10,-24 0,-11')
+        .attr('fill', '#000');
+      
+    }
+    else hideSlider = true;
   },
   watch:{
     indiCode(){
-      slider
-        .tickValues(this.activeIndicatorYears)
-        .min(this.sliderYearsArray[0])
-        .max(this.sliderYearsArray[this.sliderYearsArray.length - 1])
-        .value(this.sliderYearsArray[this.sliderYearsArray.length - 1])
-        .marks(this.sliderYearsArray)
-      sliderSVG.call(slider);
+      console.log('changed indicode', this.indiCode);
+      if (this.sliderYearsArray.length > 1 && this.indiCode !== 'region'){
+        slider
+          .tickValues(this.activeIndicatorYears)
+          .min(this.sliderYearsArray[0])
+          .max(this.sliderYearsArray[this.sliderYearsArray.length - 1])
+          .value(this.sliderYearsArray[this.sliderYearsArray.length - 1])
+          .marks(this.sliderYearsArray)
+        sliderSVG.call(slider);
+        hideSlider = false;
+      }
+      else hideSlider = true;
     }
   }
 }
 </script>
 <style>
+.sliderContainer{
+  text-align: center;
+}
 .track{
   stroke-width: 0;
 }
@@ -106,5 +145,21 @@ export default {
 fill: var(--dark-red);
 stroke-width: 0;
 }
-
+.hide-slider {
+  display: none;
+}
+.tick line{
+ display: none;
+}
+.tick text{
+  fill: #000;
+  font-size: 0.7rem;
+}
+.parameter-value text{
+  fill: #FFF;
+  font-size: 0.8rem;
+}
+.slider line{
+  opacity: 0.5;
+}
 </style>
